dd3794cb234fc64fd61f7347456f7fffba54c19a
angierobayo9809 <angierobayo09@gmail.com>
2023-02-15 20:56:34 -0500

    Ejercicio farmacia.

--- main.js
+++ main.js
@@ -1,19 +1,17 @@
-let myStyle ="background:#4FA03E;color:#8FFBEC;font-family:sans-serif;padding:5px;border:2px solid #fff;border-radius:5px;"
-let myHeaderStyle="background:#99FF;color:#551EEE;font-family:sans-serif;padding:5px;border:2px solid #7EEE7E;font-size:30px"
-let num = prompt("Ingrese un número del 1 al 10");
+let costo = 0;
+let descuento=0;
+let precioFinal=0;
 
-if (num < 1 && num > 10)
-alert("Error. Ingresó un número inválido");
-else{
-    for (let x = 1; x <= num; x++)
-    {
-        console.log(`%c Tabla del ${x}`,myHeaderStyle);
-
-        for (let multiplo = 1; multiplo <= 10; multiplo++)
-        {
-            console.log(`%c${x} * ${multiplo} =${x*multiplo}`,myStyle);
-        }
-        console.log("");
-    }
+for (let x = 1; x <= cantidad; x++)
+{
+    costo = prompt("Ingrese el costo del producto "+x);
+    descuento += (costo *10)/100;
+    precioFinal += (Number(costo));
 }
+precioFinal = precioFinal -descuento;
+
+console.log("El descuento total es de: $" + descuento);
+console.log("El precio total es de: $" + precioFinal);
+
+
 
